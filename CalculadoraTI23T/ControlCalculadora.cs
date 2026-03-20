5ab1754377c93054bc3c49742fab3e77004d169c
RENATO DAVI SOARES <renato.dsoares1@senacsp.edu.br>
2026-03-20 17:21:11 -0300

    Primeiro Commit

---
+++ CalculadoraTI23T/ControlCalculadora.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTI23T
+{
+    class ControlCalculadora
+    {
+        //Variáveis Globais
+        ModelCalculadora model;//Começando a conectar a control e a model
+
+        //Construtor vazio da classe ModelCalculadora
+        public ControlCalculadora()
+        {
+            this.model = new ModelCalculadora();//Efetivando a ligação com a Model
+        }//fim do construtor vazio
+
+        public ControlCalculadora(double num1, double num2)
+        {
+            this.model = new ModelCalculadora(num1, num2);
+        }//fim do construtor com parâmetros
+
+        //Método de Menu
+        public string mostrarMenu()
+        {
+            return "--------- Menu --------\n\n" +
+                   "Escolha uma das opções abaixo: \n" +
+                   "0. Sair\n" +
+                   "1. Somar\n" +
+                   "2. Subtrair\n" +
+                   "3. Multiplicar\n" +
+                   "4. Dividir\n" +
+                   "5. Potencia\n" +
+                   "6. Raiz Primeiro Numero\n" +
+                   "7. Raiz Primeiro Numero\n" +
+                   "8. Tabuada\n" +
+                   "9. Ano Bissexto\n" +
+                   "10. Verificação de numero";
+        }//fim do método
+
+        public void realizarOperacao()
+        {
+            int opcao = 0;//declara do lado de fora do Do...While
+            do{
+                Console.WriteLine(this.mostrarMenu());//Chamar o menu
+                opcao = Convert.ToInt32(Console.ReadLine());
+                
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        this.coletar();// Pegar oque o usuario esteja coletando  
+                        Console.WriteLine("A soma é: " + this.model.somar());
+                        break;
+                    case 2:
+                        this.coletar();// Pegar oque o usuario esteja coletando  
+                        Console.WriteLine("A subtração é: " + this.model.subtrair());
+                        break;
+                    case 3:
+                        this.coletar();// Pegar oque o usuario esteja coletando  
+                        Console.WriteLine("A multiplicação é: " + this.model.multiplicar());
+                        break;
+                    case 4:
+                        this.coletar();// Pegar oque o usuario esteja coletando  
+                        if (this.model.dividir() == -1)
+                        {
+                            Console.WriteLine("Impossível dividir por zero!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("A divisão é: " + this.model.dividir());
+                        }
+                        break;
+                    case 5:
+                        this.coletar();// Pegar oque o usuario esteja coletando  
+                        Console.WriteLine("A potencia é: " + this.model.potencia());
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe um Numero");
+                        this.model.setNum1(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("A raiz do primeiro numero é: " + this.model.raizPrimeiroNumero());
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um Numero");
+                        this.model.setNum2(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("A raiz do segundo numero é: " + this.model.raizSegundoNumero());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe um Numero");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        //para chamar a tabuada
+                        Console.WriteLine(this.model.tabuada(num));
+                        break;
+                    case 9:
+                        Console.WriteLine("Informe um ANO");
+                        int ano = Convert.ToInt32(Console.ReadLine());
+                        //Chamar o metodo do Bissexto
+                        Console.WriteLine(this.model.bissexto(ano));
+                        break;
+                    case 10:
+                        Console.WriteLine("Informe um numero para verificação");
+                        int vNum = Convert.ToInt32(Console.ReadLine());
+                        //Chamar o metodo do Verificar Numero
+                        Console.WriteLine(this.model.verificarNum(vNum));
+                        break;
+                    default:
+                        Console.WriteLine("Informe um valor entre 0 e 9");
+                        break;
+                }//fim do escolha
+            } while (opcao != 0);
+        }//fim do método
+
+        public void coletar()
+        {
+            Console.Write("\nInforme o primeiro número: ");
+            this.model.setNum1 (Convert.ToDouble(Console.ReadLine()));//Peguei o primeiro número
+
+            Console.Write("Informe o segundo número: ");
+            this.model.setNum2 (Convert.ToDouble(Console.ReadLine()));//Peguei o segundo número
+        }
+
+    }//fim da classe
+}//fim do projeto
---
+++ CalculadoraTI23T/ModelCalculadora.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTI23T
+{
+    class ModelCalculadora
+    {
+        //1º Variáveis = GLOBALMENTE = Código inteiro enxerga elas
+        private double num1;//Encapsular a variável
+        private double num2;
+        private double resultado;
+
+        //Método que instancia as variáveis = dá valores iniciais
+        //2º Método construtor
+        public ModelCalculadora()
+        {
+            this.num1      = 0;
+            this.num2      = 0;
+            this.resultado = 0;
+        }//fim do construtor
+
+        public ModelCalculadora(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }//fim do construtor com parâmetros
+
+        //3º Método GET - Consulta
+        public double getNum1()
+        {
+            return this.num1;
+        }//fim do método get
+
+        public double getNum2()
+        {
+            return this.num2;
+        }//fim do get
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }//fim do get
+
+        //4º Métodos set - Modificadores
+        public void setNum1(double num1)
+        {
+            this.num1 = num1;
+        }//fim do set
+
+        public void setNum2(double num2)
+        {
+            this.num2 = num2;
+        }//fim do set
+
+        public void setResultado(double resultado)
+        {
+            this.resultado = resultado;
+        }//fim do set
+
+        //Operações aritméticas
+        public double somar()
+        {
+            this.setResultado(this.getNum1() + this.getNum2());//Alterando a resultado
+            return this.getResultado();//Mostrando o resultado
+        }//fim do método
+
+        public double subtrair()
+        {
+            this.setResultado(this.getNum1() - this.getNum2());
+            return this.getResultado();
+        }//fim do subtrair
+
+        public double multiplicar()
+        {
+            this.setResultado(this.getNum1() * this.getNum2());
+            return this.getResultado();
+        }//fim do multiplicar
+
+        public double dividir()
+        {
+            if (this.getNum2() == 0)
+            {
+                return -1;//Flag = Porque ele vai indicar que não da para dividir por zero
+            }
+            else
+            {
+                this.setResultado(this.getNum1() / this.getNum2());
+                return this.getResultado();
+            }
+        }//fim do dividir
+
+        //POTENCIA
+
+        public double potencia() {
+            this.setResultado(Math.Pow(this.getNum1(), this.getNum2()));
+            return this.getResultado();
+        }//fim do potencia
+
+        public double raizPrimeiroNumero()
+        {
+            this.setResultado(Math.Sqrt(this.getNum1()));
+            return this.getResultado();
+        }
+
+        public double raizSegundoNumero()
+        {
+            this.setResultado(Math.Sqrt(this.getNum2()));
+            return this.getResultado();
+        }//fim do raiz Segundo numero
+
+        public string tabuada(int num)
+        {
+            string msg = "";
+            int i = 0;
+            for (i = 0; i <= 10; i++)
+            {
+                msg += "\n" + num + " * " + i + " = " + (num * i);
+
+            }//fim do for
+            return msg; 
+        }//fim do metodo
+
+        public String bissexto(int ano)
+        {
+            if (ano == 0)
+            {
+                return $"{ano} É bissexto"; //Interpolação
+            }
+            else
+            {
+                return $"{ano} NÃO é bissexto"; 
+            }
+        }
+
+        public string verificarNum(int vNum) 
+        {
+            if (vNum >= 100 &&  vNum <= 200)
+            {
+                return $"{vNum} está entre 100 e 200";
+            }
+            else
+            {
+                return $"{vNum} NÃO está entre 100 e 200";
+            }
+        }
+
+
+    }//fim da classe Model
+}//fim do projetoCalculadoraTI23T
